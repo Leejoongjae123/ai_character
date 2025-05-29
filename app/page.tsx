5da51fffa0859a9a03655af41132996ee39e385b
ljj <ljj3347@naver.com>
2025-05-29 16:55:10 +0900

    change font

--- app/page.tsx
+++ app/page.tsx
@@ -45,7 +45,7 @@ export default function Home() {
       >
         <div>
           <Button 
-            className="w-[1536px] h-[281px] text-[128px] text-[#451F0D] bg-[#E4BE50] border-5 border-[#471F0D] rounded-[60px] font-bold hover:bg-[#D4AE40] transition-colors duration-200"
+            className="w-[1550px] h-[280px] text-[128px] text-[#451F0D] bg-[#E4BE50] border-5 border-[#471F0D] rounded-[60px] font-bold hover:bg-[#D4AE40] transition-colors duration-200"
             onClick={handleClick}
           >
             화면을 눌러주세요
@@ -53,10 +53,11 @@ export default function Home() {
         </div>
         
         <div 
-          className="text-[48px] text-black drop-shadow-lg mt-[107px] mb-[214px]  font-bold"
+          className="w-[930px] h-[136px]text-[48px] text-black drop-shadow-lg mt-[107px] mb-[214px] font-bold"
+          style={{ lineHeight: '68px', letterSpacing: '-2%' }}
         >
-          <p className="text-center">얼굴을 인식하여 조선 수군으로 변신합니다.</p>
-          <p className="text-center">생성된 이미지는 포토카드로 제작할 수 있습니다.</p>
+          <p className="text-center text-[48px]">얼굴을 인식하여 조선 수군으로 변신합니다.</p>
+          <p className="text-center text-[48px]">생성된 이미지는 포토카드로 제작할 수 있습니다.</p>
         </div>
       </div>
     </div>
