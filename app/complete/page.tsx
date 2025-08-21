57ca9ba0c48f5625e22aa887294ec79056655110
ljj <ljj3347@naver.com>
2025-08-21 23:14:39 +0900

    비율조정

--- app/complete/page.tsx
+++ app/complete/page.tsx
@@ -633,7 +633,7 @@ function CompletePageContent() {
           <style>
             @page {
               /* size: 245px 386px; */
-              size: 245px 398px;
+              size: 245px 386px;
               margin: 0;
               padding: 0;
             }
@@ -663,7 +663,7 @@ function CompletePageContent() {
               /* width:245px;
               height:386px; */
               width: 245px;
-              height: 398px;
+              height: 386px;
               margin: 0;
               padding: 0;
               display: flex;
